4475aa73360d8e655fcdfc71ac559848fd1ac4ef
Ben Nelson <bgnelson25@gmail.com>
2022-01-27 22:46:50 -0600

    removing config file & installed stuff

--- server/server.js
+++ server/server.js
@@ -30,18 +30,18 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database!");
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database!", err);
-        process.exit();
-    });
+// db.mongoose
+//     .connect(db.url, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => {
+//         console.log("Connected to the database!");
+//     })
+//     .catch(err => {
+//         console.log("Cannot connect to the database!", err);
+//         process.exit();
+//     });
 
 const PORT = process.env.PORT || 5000;
 
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+eval cd ./client
+eval npm run start
+eval cd ../
+eval npm run start
+eval cd ..
